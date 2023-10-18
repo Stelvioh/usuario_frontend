aecc76035348e85f0e74e765eb82ab043eae2856
stelvio <stelvioh@gmail.com>
2023-10-18 16:00:01 -0300

    Incluído os botões para edição e remoção de usuários

--- app.js
+++ app.js
@@ -15,6 +15,18 @@ async function buscarTodosOsUsuarios() {
         novaLinha.insertCell(0).innerText = usuario.user_id;
         novaLinha.insertCell(1).innerText = usuario.name;
         novaLinha.insertCell(2).innerText = usuario.email;
+    
+        // Botões de ação
+        const colunaAcoes = novaLinha.insertCell(3);
+        const btnEditar = document.createElement("button");
+        btnEditar.innerText = "Editar";
+        btnEditar.onclick = () => editarUsuario(usuario.user_id);
+        colunaAcoes.appendChild(btnEditar);
+    
+        const btnRemover = document.createElement("button");
+        btnRemover.innerText = "Remover";
+        btnRemover.onclick = () => removerUsuario(usuario.user_id);
+        colunaAcoes.appendChild(btnRemover);
     });
 }
 
@@ -40,6 +52,43 @@ async function adicionarUsuario(evento) {
         console.error('Falha ao adicionar usuário. Servidor respondeu com', resposta.status);
     }
 }
+
+async function editarUsuario(userID) {
+    const nome = prompt("Novo nome:");
+    const email = prompt("Novo email:");
+
+    if (nome && email) {
+        const resposta = await fetch(`${url_base}/${userID}/`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ name: nome, email: email })
+        });
+
+        if (resposta.status === 200) {
+            buscarTodosOsUsuarios();
+        } else {
+            console.error('Falha ao editar usuário. Servidor respondeu com', resposta.status);
+        }
+    }
+}
+
+async function removerUsuario(userID) {
+    const confirmacao = confirm("Tem certeza que deseja remover este usuário?");
+    if (confirmacao) {
+        const resposta = await fetch(`${url_base}/${userID}/`, {
+            method: 'DELETE'
+        });
+
+        if (resposta.status === 200) {
+            buscarTodosOsUsuarios();
+        } else {
+            console.error('Falha ao remover usuário. Servidor respondeu com', resposta.status);
+        }
+    }
+}
+
 
 document.getElementById('formularioAdicionarUsuario').addEventListener('submit', adicionarUsuario);
 buscarTodosOsUsuarios();
